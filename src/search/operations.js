187d074b27037c387a9961d0bf3a2a013e898383
jfalxa <jfalxa.sta@gmail.com>
2017-03-05 18:50:37 +0100

    Added more comments

--- src/search/operations.js
+++ src/search/operations.js
@@ -10,24 +10,28 @@ export function and( left, right )
     // left AND NOT right
     if ( !isComplement( left ) && isComplement( right ) )
     {
+        // A ∩ Bc = A \ B
         return _difference( left, right[1] );
     }
 
     // NOT left AND right
     else if ( isComplement( left ) && !isComplement( right ) )
     {
+        // Ac ∩ B = B \ A
         return _difference( right, left[1] );
     }
 
     // NOT left AND NOT right
     else if ( isComplement( left ) && isComplement( right ) )
     {
+        // Ac ∩ Bc = (A ∪ B)c
         return complement( or( left[1], right[1] ) );
     }
 
     // left AND right
     else
     {
+        // A ∩ B
         return _intersection( left, right );
     }
 }
@@ -38,24 +42,28 @@ export function or( left, right )
     // left OR NOT right
     if ( !isComplement( left ) && isComplement( right ) )
     {
+        // A ∪ Bc = (Ac ∩ B)c
         return complement( and( complement( left ), right[1] ) );
     }
 
     // NOT left OR right
     else if ( isComplement( left ) && !isComplement( right ) )
     {
+        // Ac ∪ B = (A ∩ Bc)c
         return complement( and( left[1], complement( right ) ) );
     }
 
     // NOT left OR NOT right
     else if ( isComplement( left ) && isComplement( right ) )
     {
+        // Ac ∪ Bc = (A ∩ B)c
         return complement( and( left[1], right[1] ) );
     }
 
     // left OR right
     else
     {
+        // A ∪ B
         return _union( left, right );
     }
 }
--- src/search/toBinaryTree.js
+++ src/search/toBinaryTree.js
@@ -4,6 +4,7 @@ import { complement, isComplement } from './complement';
 
 function toOperand( operator, nodes )
 {
+    // recreate a full query only if there is more than one right node
     return ( nodes.length > 1 )
         ? [operator, ...nodes]
         : nodes[0];
@@ -14,17 +15,22 @@ export default function toBinaryTree( query )
 {
     if ( isLeaf( query ) )
     {
+        // stop the recursion when you reach a leaf
         return query;
     }
     else if ( isComplement( query ) )
     {
+        // in case of a NOT, keep recursing through the complemented set
         return complement( toBinaryTree( query[1] ) );
     }
 
+    // decompose the query's, members
     const [operator, leftNode, ...rightNodes] = query;
 
+    // convert the left member and go deeper in the tree with the right nodes
     const left  = toBinaryTree( leftNode );
     const right = toBinaryTree( toOperand( operator, rightNodes ) );
 
+    // combine the previously obtained binary trees into a higher binary tree
     return [operator, left, right];
 }
